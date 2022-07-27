285d1511d08a952fb37b07c4a533a0d6dc58c44e
Vivek Kumar <vivekk182btechcse2025@kccitm.edu.in>
2022-07-28 00:08:07 +0530

    to print fibonacci series

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -5,7 +5,8 @@ int main()
     int a = 0, b = 1, c, i, n;
     cout << "Enter the number of elements: ";
     cin >> n;
-    cout << a << " " << b << " ";
+    cout << a << " " << endl
+         << b << " " << endl;
     for (i = 2; i < n; i++)
     {
         c = a + b;
